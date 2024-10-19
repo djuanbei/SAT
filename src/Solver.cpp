386eac05e797efd873388cbf22bde39432287a55
Liyun Dai <dlyun2009@gmail.com>
2024-10-19 15:48:39 +0800

    0.001 version a tiny solver

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -2,7 +2,6 @@
 // Created by yun dai on 2024/10/19.
 //
 
-
 #include "Solver.h"
 
 using namespace std;
@@ -41,38 +40,255 @@ BValue simplifyClause(std::vector<Lit> &lits) {
   }
 
   return B_UN_KNOWN;
-
 }
+} // namespace
+
+bool Clause::valid() const {
+
+  if (value_.size() < 2) {
+    return false;
+  }
+  for (auto lit : value_) {
+    if (lit.isValid()) {
+      return false;
+    }
+  }
+  auto tmp = value_;
+  std::sort(tmp.begin(), tmp.end());
+
+  for (size_t i = 1; i < tmp.size(); i++) {
+    if (tmp[i - 1] == tmp[i]) {
+      return false;
+    }
+    if (isVarPairLit(tmp[i - 1], tmp[i])) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 bool Solver::add(Lit lit) {
   assert(isInitGood());
+  assert(lit.isValid());
 
   return addClauseImpl({lit});
 }
 
 bool Solver::add(Lit f, Lit s) {
   assert(isInitGood());
+  assert(f.isValid());
+  assert(s.isValid());
 
   if (f == s) {
     return addClauseImpl({f});
   }
   return addClauseImpl({f, s});
-
 }
 
 bool Solver::add(Lit f, Lit s, Lit t) {
+  assert(f.isValid());
+  assert(s.isValid());
+  assert(t.isValid());
 
   assert(isInitGood());
 
   return addClauseImpl({f, s, t});
-
 }
 bool Solver::add(std::vector<Lit> lits) {
   assert(isInitGood());
+#ifndef NDEBUG
+  for (auto lit : lits) {
+    assert(lit.isValid());
+  }
+#endif
 
   return addClauseImpl(lits);
 }
+
+SolverStatus Solver::solve() {
+  if (getStatus() != UN_SOLVE) {
+    return getStatus();
+  }
+  auto conf = propagation();
+
+  if (conf != nullptr) {
+    return UN_SAT;
+  }
+  SolverStatus s;
+
+  int number_conf = 1000;
+  while (true) {
+
+    if ((s = solveLimit(number_conf)) != UN_SOLVE) {
+      solver_status_ = s;
+      return s;
+    }
+    number_conf += 100;
+  }
+}
+
+SolverStatus Solver::solveLimit(int number_conf) {
+  int conf_n = 0;
+  Clause *conf = nullptr;
+
+  while (conf_n < number_conf) {
+    conf = propagation();
+    if (conf) {
+      /// analysis
+      auto lean = analysis(conf);
+      if (lean.second < 0) {
+        return UN_SAT;
+      }
+      backToLevel(lean.second);
+      auto lean_clause = addClause(lean.first, LEARN);
+      ///
+      addUnCheckLit(lean.first[0], lean_clause);
+
+    } else {
+      /// choose decide lit
+      auto lit = chooseOneLit();
+      if (!lit.isValid()) {
+        return SAT;
+      }
+      decide();
+      addUnCheckLit(lit, nullptr);
+    }
+  }
+
+  return UN_SOLVE;
+}
+
+Clause *Solver::propagation() {
+  while (tail_head_ < tail_.size()) {
+    auto lit = tail_[tail_head_];
+    auto neg_lit = ~lit;
+    Lit new_watch;
+    auto &ws = watch_list_[lit];
+
+    for (auto &w : ws) {
+      if (w.isRemove()) {
+        continue;
+      }
+      if (getValue(w.getOther())) {
+        // have sat
+        continue;
+      }
+      auto watch_clause = w.getClause();
+
+      if (watch_clause->getValue()[0] == neg_lit) {
+        watch_clause->getValue()[1] = neg_lit;
+      }
+      // [2,...) lit find new water lit
+      size_t i = 2;
+      for (; i < watch_clause->getValue().size(); i++) {
+        if (!isFalse(watch_clause->getValue()[i])) {
+          break;
+        }
+      }
+      if (i < watch_clause->getValue().size()) {
+        new_watch = watch_clause->getValue()[i];
+        /// remove watch clause watch_clause in lit watch list
+        /// add watch_clause in  ~new_watch watch list
+        watch_clause->getValue()[1] = new_watch;
+        watch_clause->getValue()[i] = neg_lit;
+
+        // todo
+        removeWatch(ws, watch_clause);
+
+        watch_list_[~new_watch].emplace_back(watch_clause,
+                                             watch_clause->getValue()[0]);
+
+        continue;
+      }
+      // other watch value
+      auto o_v = watch_clause->getValue()[0].getVar();
+
+      if (B_UN_KNOWN == getBValue(o_v)) {
+        addUnCheckLit(o_v, watch_clause);
+      } else {
+        // find conflict clause
+        return watch_clause;
+      }
+    }
+    tail_head_++;
+  }
+
+  return nullptr;
+}
+
+std::pair<std::vector<Lit>, int> Solver::analysis(const Clause *conf) {
+  assert(conf);
+  assert(conf->valid());
+  std::vector<char> seen(max_var_id_ + 1, 0);
+  int high_level = -1;
+
+  for (auto lit : conf->getValue()) {
+    seen[lit.getVar()] = 1;
+    if (getLevel(lit.getVar()) > high_level) {
+
+      high_level = getLevel(lit.getVar());
+    }
+  }
+
+  if (0 == high_level) {
+    return {{}, -1};
+  }
+  Lit high_level_lit;
+  for (size_t i = tail_.size(); i >= tail_limit_[high_level]; i--) {
+    auto lit = tail_[i];
+
+    if (!seen[lit.getVar()]) {
+      continue;
+    }
+
+    high_level_lit = lit;
+
+    seen[lit.getVar()] = 0;
+    auto rean_cl = reason_[lit.getIndex()];
+    if (rean_cl) {
+      for (size_t i = 1; i < rean_cl->getValue().size(); i++) {
+        auto tmp_lit = rean_cl->getValue()[i];
+        assert(!seen[tmp_lit.getVar()]);
+        seen[tmp_lit.getVar()] = 1;
+      }
+    }
+  }
+
+  std::vector<Lit> lean_clause;
+  lean_clause.push_back(~high_level_lit);
+  int second_high_leval = 0;
+
+  for (size_t v = 0; v < seen.size(); v++) {
+    if (seen[v]) {
+      if (getLevel(v) > second_high_leval) {
+        second_high_leval = v;
+      }
+
+      lean_clause.emplace_back(v, getBValue(v) == B_FASE);
+    }
+  }
+
+  return {lean_clause, second_high_leval};
+}
+
+Lit Solver::chooseOneLit() {
+  while (!var_order_.empty()) {
+    auto last = var_order_.back();
+    var_order_.pop_back();
+    if (value_[last] != B_UN_KNOWN) {
+      if (rand() % 2 == 1) {
+        return Lit(last, true);
+      } else {
+        return Lit(last, false);
+      }
+    }
+  }
+
+  return {};
+}
+
+void Solver::decide() { tail_limit_.push_back(tail_.size()); }
 
 bool Solver::addClauseImpl(std::vector<Lit> lits, ClauseOrigin origin) {
 
@@ -83,7 +299,9 @@ bool Solver::addClauseImpl(std::vector<Lit> lits, ClauseOrigin origin) {
         max_var_id_ = lit.getVar();
       }
     }
-
+    for (auto v = old_max_id; v < max_var_id_; v++) {
+      var_order_.push_back(v);
+    }
     if (max_var_id_ > old_max_id && max_var_id_ + 1 > level_.size()) {
       extandVar(1.3 * (max_var_id_ + 1) + 5);
     }
@@ -106,6 +324,14 @@ bool Solver::addClauseImpl(std::vector<Lit> lits, ClauseOrigin origin) {
     return true;
   }
 
+  addClause(lits, origin);
+
+  return true;
+}
+
+Clause *Solver::addClause(const std::vector<Lit> &lits, ClauseOrigin origin) {
+  assert(lits.size() > 1);
+
   auto cl = new Clause(lits, origin);
   clauses_.push_back(cl);
   Watcher w1(cl, lits[1]);
@@ -113,22 +339,22 @@ bool Solver::addClauseImpl(std::vector<Lit> lits, ClauseOrigin origin) {
 
   watch_list_[~lits[0]].push_back(w1);
   watch_list_[~lits[1]].push_back(w2);
-
-  return true;
-
+  return cl;
 }
 
-//todo
+// todo
 void Solver::backToLevel(int level) {
   assert(tail_limit_.size() >= level);
   if (tail_limit_.size() == level) {
     return;
   }
 
-  tail_limit_.resize(level);
-
   if (level == 0) {
     return;
+  }
+
+  for (size_t k = level; k < tail_limit_.size(); k++) {
+    var_order_.push_back(tail_[tail_limit_[k]].getVar());
   }
 
   int loc = tail_limit_[level - 1];
@@ -138,8 +364,10 @@ void Solver::backToLevel(int level) {
     value_[lit.getVar()] = B_UN_KNOWN;
   }
 
-  tail_.resize(loc);
+  tail_limit_.resize(level);
 
+  tail_.resize(loc);
+  tail_head_ = tail_.size();
 }
 //// return false iff it find the lits is false
 BValue Solver::simplifyForInput(std::vector<Lit> &lits) const {
@@ -164,7 +392,6 @@ BValue Solver::simplifyForInput(std::vector<Lit> &lits) const {
   it++;
   lits.resize(it - lits.begin());
   return lits.empty() ? B_FASE : B_UN_KNOWN;
-
 }
 
 void Solver::extandVar(int new_max_num) {
@@ -181,7 +408,6 @@ void Solver::resetStatus() {
       tail_.erase(tail_.begin() + tail_limit_[0], tail_.end());
     }
     tail_limit_.clear();
-
     solver_status_ = UN_SOLVE;
   }
 }
@@ -196,6 +422,4 @@ std::vector<BValue> Solver::getModel() const {
     return model;
   }
   return {};
-
 }
-
--- src/Solver.h
+++ src/Solver.h
@@ -4,36 +4,33 @@
 
 #ifndef SAT_SRC_SOLVER_H_
 #define SAT_SRC_SOLVER_H_
-#include <vector>
 #include <cassert>
 #include <map>
+#include <vector>
 
 namespace sat {
 
-enum BValue {
-  B_TRUE = 0,
-  B_FASE,
-  B_UN_KNOWN
-};
+enum BValue { B_TRUE = 0, B_FASE, B_UN_KNOWN };
 
 static inline BValue operator~(BValue v) {
   switch (v) {
-    case B_TRUE:return B_FASE;
-    case B_FASE:return B_TRUE;
-    case B_UN_KNOWN:return B_UN_KNOWN;
+  case B_TRUE:
+    return B_FASE;
+  case B_FASE:
+    return B_TRUE;
+  case B_UN_KNOWN:
+    return B_UN_KNOWN;
   }
 }
 
-static inline int weakValue(BValue v) {
-  return v == B_FASE ? 0 : 1;
-}
+static inline int weakValue(BValue v) { return v == B_FASE ? 0 : 1; }
 
 class Lit {
 
-  int var_{0}; //start from 0
+  int var_{-1}; // start from 0
   bool sign_{false};
 
- public:
+public:
   Lit() = default;
   Lit(int v, bool s = false) {
     assert(v >= 0);
@@ -41,25 +38,18 @@ class Lit {
     sign_ = s;
   }
 
-  int getVar() const {
-    return var_;
-  }
+  int getVar() const { return var_; }
 
-  bool getSign() const {
-    return sign_;
-  }
-  int getIndex() const {
-    return 2 * var_ + (sign_ ? 1 : 0);
-  }
-  BValue getVarBValue() const {
-    return getSign() ? B_FASE : B_TRUE;
-
-  }
+  bool getSign() const { return sign_; }
+  int getIndex() const { return 2 * var_ + (sign_ ? 1 : 0); }
+  BValue getVarBValue() const { return getSign() ? B_FASE : B_TRUE; }
+  bool isValid() const { return var_ >= 0; }
   bool operator<(const Lit &other) const {
-    return std::pair<int, bool>(var_, sign_) < std::pair<int, bool>(other.var_, other.sign_);
+    return std::pair<int, bool>(var_, sign_) <
+           std::pair<int, bool>(other.var_, other.sign_);
   }
- private:
 
+private:
 };
 
 static inline bool operator==(const Lit lhs, const Lit rhs) {
@@ -74,6 +64,10 @@ static inline Lit operator~(Lit lit) {
   return Lit(lit.getVar(), !lit.getSign());
 }
 
+static inline bool isVarPairLit(Lit lhs, Lit rhs) {
+  return lhs.getVar() == rhs.getVar() && (lhs.getSign() != rhs.getSign());
+}
+
 static inline int weakValue(Lit lit, BValue v) {
   if (lit.getSign()) {
     return v == B_TRUE ? 0 : 1;
@@ -81,130 +75,113 @@ static inline int weakValue(Lit lit, BValue v) {
   return v == B_FASE ? 0 : 1;
 }
 
-enum ClauseOrigin {
-  FORMULA = 0,
-  LEARN
-};
+enum ClauseOrigin { FORMULA = 0, LEARN };
 
 class Clause {
 
   ClauseOrigin origin_{FORMULA};
   std::vector<Lit> value_;
 
- public:
-
+public:
   Clause() = default;
-  Clause(std::vector<Lit> lits) : value_(std::move(lits)) {
+  Clause(std::vector<Lit> lits) : value_(std::move(lits)) {}
 
-  }
+  Clause(std::vector<Lit> lits, ClauseOrigin o)
+      : origin_(o), value_(std::move(lits)) {}
 
-  Clause(std::vector<Lit> lits, ClauseOrigin o) : origin_(o), value_(std::move(lits)) {
+  ClauseOrigin getOrigin() const { return origin_; }
 
-  }
+  const std::vector<Lit> &getValue() const { return value_; }
 
-  ClauseOrigin getOrigin() const {
-    return origin_;
-  }
+  std::vector<Lit> &getValue() { return value_; }
 
-  const std::vector<Lit> &getValue() const {
-    return value_;
-  }
+  bool valid() const;
 
- private:
-
+private:
 };
 
 class Watcher {
 
-  const Clause *clause_{nullptr};
+  Clause *clause_{nullptr};
 
   Lit other_watch_lit_;
 
- public:
+  bool is_remove_{false};
+
+public:
   Watcher() = default;
-  Watcher(const Clause *c, Lit other) : clause_(c), other_watch_lit_(other) {
+  Watcher(Clause *c, Lit other) : clause_(c), other_watch_lit_(other) {}
+  Clause *getClause() const { return clause_; }
 
-  }
-  const Clause *getClause() const {
-    return clause_;
-  }
+  Lit getOther() const { return other_watch_lit_; }
+  void remove() { is_remove_ = true; }
+  bool isRemove() const { return is_remove_; }
 
-  Lit getOther() const {
-    return other_watch_lit_;
-  }
+  bool valid() const { return clause_; }
 
-  bool valid() const {
-    return clause_;
-  }
-
- private:
-
+private:
 };
 
-enum SolverStatus {
-  UN_SOLVE,
-  SAT,
-  UN_SAT
-};
+enum SolverStatus { UN_SOLVE, SAT, UN_SAT };
+
+static inline void removeWatch(std::vector<Watcher> &ws, const Clause *cl) {
+  for (auto &w : ws) {
+    if (!w.isRemove() && w.getClause() == cl) {
+      w.remove();
+      // ws.erase(it);
+      return;
+    }
+  }
+}
 
 class Solver {
 
   int max_var_id_{0};
 
   std::vector<Clause *> clauses_;
-  std::map<Lit, std::vector<Watcher> > watch_list_;
+  std::map<Lit, std::vector<Watcher>> watch_list_;
 
   int tail_head_{0};
   std::vector<Lit> tail_;
   std::vector<int> tail_limit_;
-  std::vector<BValue> value_;// Var ->
-  std::vector<int> level_;// Var ->
+  std::vector<BValue> value_; // Var ->
+  std::vector<int> level_;    // Var ->
+  std::vector<int> var_order_;
 
   ////Lit index,  Var v  --> 2*v ; ~v--> 2v+1
   // Lit ->
-  std::vector<const Clause *> reason_; //lit --> reason cl (cl --> lit)
-
-
+  std::vector<const Clause *> reason_; // lit --> reason cl (cl --> lit)
 
   SolverStatus solver_status_{UN_SOLVE};
 
- public:
-
+public:
   bool add(Lit lit);
   bool add(Lit f, Lit s);
   bool add(Lit f, Lit s, Lit t);
   bool add(std::vector<Lit> lits);
 
-  int getVarNum() const {
-    return max_var_id_ + 1;
-  }
+  int getVarNum() const { return max_var_id_ + 1; }
 
-  int getClauseNum() const {
-    return clauses_.size();
-  }
+  int getClauseNum() const { return clauses_.size(); }
 
   SolverStatus solve();
 
   std::vector<BValue> getModel() const;
 
-  SolverStatus getStatus() const {
-    return solver_status_;
-  }
+  SolverStatus getStatus() const { return solver_status_; }
 
-  bool isGood() const {
-    return getStatus() != UN_SAT;
-  }
+  bool isGood() const { return getStatus() != UN_SAT; }
 
   void resetStatus();
 
- private:
+private:
+  SolverStatus solveLimit(int number_conf);
+
   void extandVar(int new_max_num);
 
-  int getCurrentLevel() const {
-    return tail_limit_.size();
-  }
+  int getCurrentLevel() const { return tail_limit_.size(); }
 
-//// return false iff it find the lits is false
+  //// return false iff it find the lits is false
   BValue simplifyForInput(std::vector<Lit> &lits) const;
 
   void addUnCheckLit(Lit lit, const Clause *cl) {
@@ -214,21 +191,37 @@ class Solver {
     reason_[lit.getIndex()] = cl;
   }
 
-  const Clause *propagation();
+  Clause *propagation();
+
+  std::pair<std::vector<Lit>, int> analysis(const Clause *conf);
+
+  Lit chooseOneLit();
+
+  void decide();
 
   void backToLevel(int level);
 
-  bool isInitGood() const {
-    return isGood() && isInitStatus();
+  BValue getBValue(int v) const { return value_[v]; }
+  bool isFalse(Lit lit) const {
+    if (lit.getSign()) {
+      return value_[lit.getVar()] == B_TRUE;
+    }
+    return value_[lit.getVar()] == B_FASE;
   }
 
-  bool isInitStatus() const {
-    return tail_limit_.empty();
+  bool getValue(Lit lit) const {
+    return value_[lit.getVar()] == lit.getVarBValue();
   }
 
+  int getLevel(int v) const { return level_[v]; }
+
+  bool isInitGood() const { return isGood() && isInitStatus(); }
+
+  bool isInitStatus() const { return tail_limit_.empty(); }
   bool addClauseImpl(std::vector<Lit> lits, ClauseOrigin origin = FORMULA);
 
+  Clause *addClause(const std::vector<Lit> &lits, ClauseOrigin origin);
 };
-}
+} // namespace sat
 
-#endif //SAT_SRC_SOLVER_H_
+#endif // SAT_SRC_SOLVER_H_
